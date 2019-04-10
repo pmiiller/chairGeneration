32cc4ec2df0935dd5f5da46a10819a919359d9a0
diogogimenes <digogim@gmail.com>
2019-04-09 22:20:48 -0700

    fixed output not being sorted, the output array now correlates to each obj in order

--- evaluate_sample.py
+++ evaluate_sample.py
@@ -7,7 +7,7 @@ from model import cnn_model_fn
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-def load(dimension, directory = "evaluate-chairs/"):
+def load(dimension, directory = "new_chair_bmp/"):
 
     imagesTop = []
     imagesSide = []
@@ -19,15 +19,25 @@ def load(dimension, directory = "evaluate-chairs/"):
     length = len(os.listdir(folder)) // 3
     ls += length
 
-    for filename in os.listdir(folder):
+    fileList = os.listdir(folder)
+    fileList.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+    print(fileList)
+
+
+    # for filename in os.listdir(folder):
+    for i in range(len(fileList)):
+        filename = fileList[i]
+    #     print(i)
 
         view = int(filename.split(".")[0])
+        print(view)
         view = view % 3
         img = cv2.imread(folder+filename)
         if dimension < 224:
             img = cv2.resize(img, dsize=(dimension, dimension), interpolation=cv2.INTER_CUBIC)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = np.nan_to_num(img)
+
 
         if img is not None:
             if view == 1:
@@ -36,6 +46,7 @@ def load(dimension, directory = "evaluate-chairs/"):
                 imagesTop.append(1. - img / 255.)
             else:
                 imagesFront.append(1. - img / 255.)
+
 
     imagesTop = np.array(imagesTop)
     imagesFront = np.array(imagesFront)
@@ -65,7 +76,9 @@ def main(*argv):
     test_evaluations = [[],[],[]]
 
     for view in ["Top", "Front", "Side"]:
+        print(view)
         id = ["Top", "Front", "Side"].index(view)
+
 
         classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="checkpoint/"+view+"/")
 
@@ -80,12 +93,16 @@ def main(*argv):
         #This line should print something like {'classes': 0, 'probabilities': array([0.91087427, 0.08912573])}
         #the first element of 'probabilities' is the correlation of input with the Negative samples. The second element means positive.
         #If you evaluate multiple samples, just keep iterating over the eval_results generator.
-        #eval_instance = next(eval_results)
-        #print(eval_instance)
+        # eval_instance = next(eval_results)
+        # print("eval_instance: ")
+        # print(eval_instance)
 
         # This is how you extract the correlation to the positive class of the first element in your evaluation folder
         for eval in eval_results:
             #print("probability that this instance is positive is %3.2f " % eval['probabilities'][1])
+            print(view)
+            print(id)
+            print(eval)
             test_evaluations[id].append(eval['probabilities'][1])
 
     #the probability that the chair is a positive example is given by the minimum of the probabilities from each of the three views
@@ -93,8 +110,10 @@ def main(*argv):
     #as can be seen in this quick evaluation, there is roon for inprovement in the algorithm
     evaluation_chairs = np.amin(test_evaluations, axis=0)
     print(evaluation_chairs)
+    print("done!")
 
     return evaluation_chairs
+
 
 if __name__ == "__main__":
     # Add ops to save and restore all the variables.
